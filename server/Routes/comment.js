88b629a3608eb55d71603d69df98d6309cc59de7
Aswin Ak <aswinak1o1@gmail.com>
2024-11-06 10:07:20 +0530

    first commit

--- server/Routes/comment.js
+++ server/Routes/comment.js
@@ -1,12 +1,17 @@
-import express from "express"
+import express from "express";
 
-import { postcomment,getcomment,deletecomment,editcomment } from "../Controllers/Comment.js"
-import auth from "../middleware/auth.js"
-const router=express.Router()
+import {
+  postcomment,
+  getcomment,
+  deletecomment,
+  editcomment,
+} from "../Controllers/Comment.js";
+import auth from "../Middleware/auth.js";
+const router = express.Router();
 
-router.post("/post",auth,postcomment)
-router.get('/get',getcomment)
-router.delete('/delete/:id',auth,deletecomment)
-router.patch('/edit/:id',auth,editcomment)
+router.post("/post", auth, postcomment);
+router.get("/get", getcomment);
+router.delete("/delete/:id", auth, deletecomment);
+router.patch("/edit/:id", auth, editcomment);
 
-export default router
+export default router;
